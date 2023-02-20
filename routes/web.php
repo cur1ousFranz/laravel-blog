32235e831ee5263c8840f5512d37a33a5f513529
cur1ousFranz <xfranjeff@gmail.com>
2023-02-21 02:14:34 +0800

    question / admin routes

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,23 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+use App\Http\Controllers\Auth\UserController;
+use App\Http\Controllers\Questions\QuestionController;
 
 Route::get('/', function () {
     return view('welcome');
+})->name('home');
+
+Route::get('/authenticate', [UserController::class, 'index'])->name('login')->middleware('guest');
+Route::post('/authenticate', [UserController::class, 'store'])->name('authenticate');
+
+Route::group(['middleware' => 'auth'], function() {
+
+    Route::get('/signout', [UserController::class, 'destroy'])->name('signout');
+
+    Route::get('/question', [QuestionController::class, 'index'])->name('question-index');
+    Route::post('/question', [QuestionController::class, 'store'])->name('question-create');
+    Route::get('/question/edit/{question:slug}', [QuestionController::class, 'edit'])->name('question-edit');
+    Route::put('/question/edit/{question:slug}', [QuestionController::class, 'update'])->name('question-update');
 });
+Route::get('/question/{question:slug}', [QuestionController::class, 'show'])->name('question-show');
