762cf68ccf91ddc627e806e213eac956bb9fe29a
cur1ousFranz <xfranjeff@gmail.com>
2023-02-22 02:16:48 +0800

    search / about / update question

--- routes/web.php
+++ routes/web.php
@@ -4,11 +4,10 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\UserController;
 use App\Http\Controllers\Questions\QuestionController;
 
-Route::get('/', function () {
-    return view('welcome');
-})->name('home');
+Route::get('/', [UserController::class, 'index'])->name('home');
+Route::get('/about', [UserController::class, 'about'])->name('about');
 
-Route::get('/authenticate', [UserController::class, 'index'])->name('login')->middleware('guest');
+Route::get('/authenticate', [UserController::class, 'show'])->name('login')->middleware('guest');
 Route::post('/authenticate', [UserController::class, 'store'])->name('authenticate');
 
 Route::group(['middleware' => 'auth'], function() {
@@ -18,6 +17,7 @@ Route::group(['middleware' => 'auth'], function() {
     Route::get('/question', [QuestionController::class, 'index'])->name('question-index');
     Route::post('/question', [QuestionController::class, 'store'])->name('question-create');
     Route::get('/question/edit/{question:slug}', [QuestionController::class, 'edit'])->name('question-edit');
-    Route::put('/question/edit/{question:slug}', [QuestionController::class, 'update'])->name('question-update');
+    Route::put('/question/{question:slug}', [QuestionController::class, 'update'])->name('question-update');
 });
 Route::get('/question/{question:slug}', [QuestionController::class, 'show'])->name('question-show');
+Route::post('/question/search', [QuestionController::class, 'searchQuestion'])->name('question-search');
