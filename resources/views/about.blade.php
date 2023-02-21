37bc96c32df9d1b832a44422a5d45bda3ff009b6
cur1ousFranz <xfranjeff@gmail.com>
2023-02-22 02:14:34 +0800

    about page

---
+++ resources/views/about.blade.php
@@ -0,0 +1,38 @@
+<x-layouts.layout title="About">
+    <div class="px-4 mt-8">
+        <div class="w-full flex">
+            <div class="hidden w-96 md:block">
+                {{-- ADS --}}
+            </div>
+            <div class="w-full p-4 shadow-sm">
+                <div class="mb-4">
+                    {{-- ADS --}}
+                </div>
+                <div>
+                    <h1 class="text-2xl font-bold mb-2 text-gray-800">About Us</h1>
+                    <p class="text-lg text-gray-800">
+                        Welcome to Snippet, the ultimate destination for coding enthusiasts and developers seeking solutions for their coding problems. We are a team of passionate writers, software engineers, and tech enthusiasts committed to bringing you the latest updates and solutions to help you stay ahead in the world of programming.
+                        <br><br>
+                        Our blog provides a platform for developers to learn, grow, and connect with other like-minded individuals in the community. Whether you are looking to hone your skills, discover new programming concepts or solve real-world coding problems, Snippet is your one-stop-shop.
+                        <br><br>
+                        We pride ourselves on delivering high-quality content that is not only engaging but also informative. Our team of experts works diligently to provide you with the latest industry news, trends, and insights to keep you informed and up-to-date.
+                        <br><br>
+                        At Snippet, we value our readers and are committed to building a strong community that supports one another. We encourage you to leave comments, engage with our writers, and share your own knowledge with others.
+                        <br><br>
+                        Thank you for choosing Snippet as your go-to destination for coding solutions. We are excited to have you as a part of our community, and we look forward to helping you grow your programming skills.
+                        <br><br>
+                        Sincerely,
+                        <br><br>
+                        The Snippet Team
+                    </p>
+                </div>
+                <div class="mt-6">
+                    {{-- ADS --}}
+                </div>
+            </div>
+            <div class="hidden w-96 md:block">
+                 {{-- ADS --}}
+            </div>
+        </div>
+    </div>
+</x-layouts.layout>
