556f76a71af700c86fc3f25a36f4b352e064bb72
cur1ousFranz <xfranjeff@gmail.com>
2023-02-27 02:05:37 +0800

    add tag and disclaimer route

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Questions\QuestionController;
 
 Route::get('/', [UserController::class, 'index'])->name('home');
 Route::get('/about', [UserController::class, 'about'])->name('about');
+Route::get('/disclaimer', [UserController::class, 'disclaimer'])->name('disclaimer');
 
 Route::get('/authenticate', [UserController::class, 'show'])->name('login')->middleware('guest');
 Route::post('/authenticate', [UserController::class, 'store'])->name('authenticate');
@@ -20,5 +21,6 @@ Route::group(['middleware' => 'auth'], function() {
     Route::put('/question/{question:slug}', [QuestionController::class, 'update'])->name('question-update');
 });
 Route::get('/question/{question:slug}', [QuestionController::class, 'show'])->name('question-show');
+Route::get('/question/tag/{tag}', [QuestionController::class, 'showQuestionsByTag'])->name('question-tag');
 Route::post('/question/search', [QuestionController::class, 'searchQuestion'])->name('question-search');
 Route::get('/category/{category}', [QuestionController::class, 'category'])->name('category');
