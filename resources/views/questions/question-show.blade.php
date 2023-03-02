dade365c3673a71a1109c56c0868df18c3f9e855
cur1ousFranz <xfranjeff@gmail.com>
2023-03-02 22:32:05 +0800

    validate related random question total

--- resources/views/questions/question-show.blade.php
+++ resources/views/questions/question-show.blade.php
@@ -56,9 +56,23 @@
                 <div class="p-6 shadow-sm border rounded-md border-t-2 border-t-gray-400" style="margin-top: 50px">
                     <h1 class="text-xl font-bold text-gray-800 md:text-3xl">Related problems</h1>
                     <div class="mt-4">
-                        @if ($questions)
+                        @if ($questions->count() >= 10)
                             <ul>
                                 @foreach ($questions->random(10) as $question)
+                                    <li class="truncate border-b border-gray-200 p-2 max-w-full hover:bg-gray-200" >
+                                        <span class="inline-block">
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" fill="currentColor" class="bi bi-arrow-up-right-square" viewBox="0 0 16 16">
+                                                <path fill-rule="evenodd" d="M15 2a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2zM0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2zm5.854 8.803a.5.5 0 1 1-.708-.707L9.243 6H6.475a.5.5 0 1 1 0-1h3.975a.5.5 0 0 1 .5.5v3.975a.5.5 0 1 1-1 0V6.707l-4.096 4.096z"/>
+                                            </svg>
+                                        </span>
+                                        <a href="{{ route('question-show', [$question->slug]) }}" target="_blank" class=" w-full font-semibold">{{ $question->title }}</a>
+                                    </li>
+                                @endforeach
+                            </ul>
+                        @endif
+                        @if ($question->count() > 0)
+                            <ul>
+                                @foreach ($questions as $question)
                                     <li class="truncate border-b border-gray-200 p-2 max-w-full hover:bg-gray-200" >
                                         <span class="inline-block">
                                             <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" fill="currentColor" class="bi bi-arrow-up-right-square" viewBox="0 0 16 16">
