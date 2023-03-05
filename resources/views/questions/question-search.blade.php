c4dd4f77c0002aceed7530d129d57e0480d68998
cur1ousFranz <xfranjeff@gmail.com>
2023-03-05 19:59:08 +0800

    add ui container for search/related questions

--- resources/views/questions/question-search.blade.php
+++ resources/views/questions/question-search.blade.php
@@ -13,24 +13,23 @@
                         <p class="text-red-500 w-full absolute text-sm">{{ $message }}</p>
                     @enderror
                 </form>
-                <div class="mt-4">
+                <div class="mt-12">
                     @if ($questions->count())
-                        @foreach ($questions as $question)
-                            <div class="p-2 max-w-full rounded hover:bg-gray-200" >
-                                <span class="inline-block">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" fill="currentColor" class="bi bi-arrow-up-right-square" viewBox="0 0 16 16">
-                                        <path fill-rule="evenodd" d="M15 2a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2zM0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2zm5.854 8.803a.5.5 0 1 1-.708-.707L9.243 6H6.475a.5.5 0 1 1 0-1h3.975a.5.5 0 0 1 .5.5v3.975a.5.5 0 1 1-1 0V6.707l-4.096 4.096z"/>
-                                    </svg>
-                                </span>
-                                <a href="{{ route('question-show', [$question->slug]) }}" target="_blank" class=" w-full font-semibold underline hover:cursor-pointer text-blue-500 hover:text-blue-700">{{ $question->title }}</a>
-                            </div>
-                        @endforeach
+                        <div class="grid grid-cols-2 lg:grid-cols-3 gap-4">
+                            @foreach ($questions as $question)
+                                @php
+                                    $description = preg_match('/^([^\.!?]*[\.!?]+){2}/', $question->body, $matches);
+                                    $description = $matches[0];
+                                    $description = preg_replace('/<[^>]*>/', '', $description);
+                                @endphp
+                                <x-question-container :question="$question" :description="$description"/>
+                            @endforeach
+                        </div>
                     @else
                         <h4 class="text-lg mt-4 text-center font-bold text-gray-500">
                             No result keyword "{{ $search }}".
                         </h4>
                     @endif
-                    {{-- {{ $questions->links('pagination::semantic-ui')}}  --}}
                 </div>
 
                 <div class="mt-6">
--- resources/views/questions/question-show.blade.php
+++ resources/views/questions/question-show.blade.php
@@ -58,32 +58,28 @@
                 <div class="p-6 shadow-sm border rounded-md border-t-2 border-t-gray-400" style="margin-top: 50px">
                     <h1 class="text-xl font-bold text-gray-800 md:text-3xl">Related problems</h1>
                     <div class="mt-4">
-                        @if ($questions->count() >= 10)
-                            <ul>
-                                @foreach ($questions->random(10) as $question)
-                                    <li class="truncate border-b border-gray-200 p-2 max-w-full hover:bg-gray-200" >
-                                        <span class="inline-block">
-                                            <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" fill="currentColor" class="bi bi-arrow-up-right-square" viewBox="0 0 16 16">
-                                                <path fill-rule="evenodd" d="M15 2a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2zM0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2zm5.854 8.803a.5.5 0 1 1-.708-.707L9.243 6H6.475a.5.5 0 1 1 0-1h3.975a.5.5 0 0 1 .5.5v3.975a.5.5 0 1 1-1 0V6.707l-4.096 4.096z"/>
-                                            </svg>
-                                        </span>
-                                        <a href="{{ route('question-show', [$question->slug]) }}" target="_blank" class=" w-full font-semibold">{{ $question->title }}</a>
-                                    </li>
+                        @if ($questions->count() >= 12)
+                            <div class="grid grid-cols-2 lg:grid-cols-3 gap-4">
+                                @foreach ($questions->random(12) as $question)
+                                    @php
+                                        $description = preg_match('/^([^\.!?]*[\.!?]+){2}/', $question->body, $matches);
+                                        $description = $matches[0];
+                                        $description = preg_replace('/<[^>]*>/', '', $description);
+                                    @endphp
+                                    <x-question-container :question="$question" :description="$description"/>
                                 @endforeach
-                            </ul>
+                            </div>
                         @elseif($question->count() > 0)
-                            <ul>
+                            <div class="grid grid-cols-2 lg:grid-cols-3 gap-4">
                                 @foreach ($questions as $question)
-                                    <li class="truncate border-b border-gray-200 p-2 max-w-full hover:bg-gray-200" >
-                                        <span class="inline-block">
-                                            <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" fill="currentColor" class="bi bi-arrow-up-right-square" viewBox="0 0 16 16">
-                                                <path fill-rule="evenodd" d="M15 2a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2zM0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2zm5.854 8.803a.5.5 0 1 1-.708-.707L9.243 6H6.475a.5.5 0 1 1 0-1h3.975a.5.5 0 0 1 .5.5v3.975a.5.5 0 1 1-1 0V6.707l-4.096 4.096z"/>
-                                            </svg>
-                                        </span>
-                                        <a href="{{ route('question-show', [$question->slug]) }}" target="_blank" class=" w-full font-semibold">{{ $question->title }}</a>
-                                    </li>
+                                    @php
+                                        $description = preg_match('/^([^\.!?]*[\.!?]+){2}/', $question->body, $matches);
+                                        $description = $matches[0];
+                                        $description = preg_replace('/<[^>]*>/', '', $description);
+                                    @endphp
+                                    <x-question-container :question="$question" :description="$description"/>
                                 @endforeach
-                            </ul>
+                            </div>
                         @else 
                             <h4 class="text-lg mt-4 text-center font-bold text-gray-500">Nothing to show.</h4>
                         @endif
@@ -96,9 +92,3 @@
         </div>
     </div>
 </x-layouts.layout>
-
-<style>
-    a { 
-        color: blue;
-    }
-</style>
