b44d654fef536c137f28ee4d24f9928556e80385
cur1ousFranz <xfranjeff@gmail.com>
2023-03-16 20:34:51 +0800

    random related posts

--- resources/views/questions/question-show.blade.php
+++ resources/views/questions/question-show.blade.php
@@ -106,9 +106,22 @@
 
                 <div class="p-4 rounded-md shadow-sm border border-t-2 border-t-gray-400">
                     <h1 class="text-lg mb-3 font-bold text-gray-800 md:text-xl">Related</h1>
-                    @if ($questions)
+                    @if ($questions->count() >= 7)
                         <ul>
-                            @foreach ($questions->random(5) as $question)
+                            @foreach ($questions->random(7) as $question)
+                                <li class="truncate text-sm border-b border-gray-200 p-2 max-w-full hover:bg-gray-200" >
+                                    <span class="inline-block">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" fill="currentColor" class="bi bi-arrow-up-right-square" viewBox="0 0 16 16">
+                                            <path fill-rule="evenodd" d="M15 2a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2zM0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2zm5.854 8.803a.5.5 0 1 1-.708-.707L9.243 6H6.475a.5.5 0 1 1 0-1h3.975a.5.5 0 0 1 .5.5v3.975a.5.5 0 1 1-1 0V6.707l-4.096 4.096z"/>
+                                        </svg>
+                                    </span>
+                                    <a href="{{ route('question-show', [$question->slug]) }}" class=" text-blue-500 w-full font-semibold">{{ $question->title }}</a>
+                                </li>
+                            @endforeach
+                        </ul>
+                    @elseif($question->count() > 0)
+                        <ul>
+                            @foreach ($questions as $question)
                                 <li class="truncate text-sm border-b border-gray-200 p-2 max-w-full hover:bg-gray-200" >
                                     <span class="inline-block">
                                         <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" fill="currentColor" class="bi bi-arrow-up-right-square" viewBox="0 0 16 16">
